54d3d1e4f793203e9725aebbff50c21384f7b01e
Rizsid <rizsid16@gmail.com>
2024-11-08 16:50:49 +0500

    basic styling

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,4 +1,4 @@
-import { FlatList, StyleSheet, Text, View } from 'react-native'
+import { FlatList, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import CustomScreen from '../components/CustomScreen'
 import CustomText from '../components/CustomText'
@@ -7,30 +7,39 @@ import useApiManager from '../customHooks/useApiManager'
 const HomeScreen = () => {
 
     const { fnGetApi, isLoading } = useApiManager();
-    
+
     const [allMovies, setAllMovies] = useState([]);
 
-    useEffect(()=> { fnGetAllMovies() },[]);
-    
-    const fnGetAllMovies =async()=>{
+    useEffect(() => { fnGetAllMovies() }, []);
+
+    const fnGetAllMovies = async () => {
         const data = await fnGetApi();
         setAllMovies(data?.results);
     };
 
+    const ViewMovieItem = ({ item }) => {
+        return (
+            <TouchableOpacity activeOpacity={0.7} style={styles.listItemBox} >
+                <Image source={{uri: item?.artworkUrl100}} style={styles.movieImg} />
+                <CustomText>
+                    {item?.artistName}
+                </CustomText>
+            </TouchableOpacity>
+        )
+    }
+
     return (
         <CustomScreen>
             <CustomText>
-                Movies
+                All Movies
             </CustomText>
             <FlatList
                 data={allMovies}
                 keyExtractor={(item, index) => index.toString()}
-                renderItem={({ item })=>(
-                        <CustomText>
-                            {item?.shortDescription}
-                        </CustomText>
-                    )
-                }
+                numColumns={2}
+                showsVerticalScrollIndicator={false}
+                columnWrapperStyle={styles.listColumn}
+                renderItem={ViewMovieItem}
             />
         </CustomScreen>
     )
@@ -38,4 +47,8 @@ const HomeScreen = () => {
 
 export default HomeScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    listColumn : {justifyContent:'space-around', gap:16, marginTop:16},
+    listItemBox : {flex:1, alignItems:'center', gap:4},
+    movieImg : {height:260, width: '100%', borderRadius:8}
+})
