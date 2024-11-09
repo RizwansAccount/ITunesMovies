021dd70e1833781bbccb85e0f397bdadad450a94
Rizsid <rizsid16@gmail.com>
2024-11-10 02:11:36 +0500

    changes

--- src/components/CustomImage.jsx
+++ src/components/CustomImage.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import FastImage from 'react-native-fast-image'
 
-const CustomImage = ({source, style}) => {
+const CustomImage = ({ source, style }) => {
+  const [isFailToLoad, setIsFailToLoad] = useState(false);
   return (
-    <FastImage source={source} style={style} />
+    <FastImage
+      source={isFailToLoad ? require('../../assets/images/empty_img.png') : source}
+      style={[style, isFailToLoad && { height: 100, width: 100 }]}
+      onError={() => setIsFailToLoad(true)}
+    />
   )
 }
 
